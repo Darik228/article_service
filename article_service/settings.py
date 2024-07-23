24073e807e0981be9cfa36bd5c407e1c5b24e0dd
serdar <s.sherimkulov@gmail.com>
2024-07-23 10:26:53 +0200

    changes

--- article_service/settings.py
+++ article_service/settings.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-u5v@987z2qw3=zb+eh=jp9!ufzsv6#wf0+_z8m6s^(--+ehjd7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['article_service.herokuapp.com']
 
 
 # Application definition
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'article_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dfrfjsnmhgvi7j',
+        'USER': 'u7203j5ojtuh4e',
+        'PASSWORD': 'p23bb44364f3b2848f244788f12b342b0dd9836be9857194b69cfe48ebd8bc018',
+        'HOST': 'cah8ha8ra8h8i7.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
@@ -115,7 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
